59b84d78e9a24e5ef945604380156f8631d50392
yamikjg4 <yamikgandhi@outlook.com>
2022-02-21 13:03:38 +0530

    new changes

---
+++ Partical/Controllers/DefaulltController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Partical.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Partical.Controllers
+{
+    public class DefaulltController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> std=new List<Student>(){
+            new Student{ sid=1,sname="Yamik",degree="MCA"},
+            new Student{ sid=2,sname="Kavi",degree="Bca"},
+            new Student{ sid=3,sname="Raj",degree="BBA"},
+            new Student{ sid=4,sname="Mahesh",degree="BCOM"},
+            new Student{ sid=5,sname="Rajesh",degree="MCOM"},
+            };
+            return View(std);
+        }
+    }
+}
---
+++ Partical/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Partical.Models
+{
+    public class Student
+    {
+        public int sid { get; set; }
+        public string sname { get; set; }
+        public string degree { get; set; }
+    }
+}
